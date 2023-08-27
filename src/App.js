ed4ebbbaa45da46464474ffb0530a83cc81812bd
Sumanth3028 <sumanthakula05@gmail.com>
2023-08-27 23:12:29 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SignupForm from './components/Auth/SignupForm';
+import LoginForm from './components/Auth/LoginForm';
+import { Route, Routes } from 'react-router-dom';
+
+import TaskList from './components/Task/TaskList';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-full  bg-[#ffffff]'>
+
+   <Routes>
+    <Route path='/login' element={<LoginForm/>} />
+    <Route path='/' element={<SignupForm/>} />
+    <Route path='/task' element={<TaskList/>} />
+   </Routes>
+
     </div>
   );
 }
+
 
 export default App;
